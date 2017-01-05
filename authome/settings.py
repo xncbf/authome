21b8582314014399a76256204621715c303cc384
xncbf <xncbf12@gmail.com>
2017-01-05 22:42:28 +0900

    세팅파일 수정

--- authome/settings.py
+++ authome/settings.py
@@ -14,7 +14,7 @@ import os
 from unipath import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).ancestor(3)
+BASE_DIR = Path(__file__).ancestor(2)
 
 
 # Quick-start development settings - unsuitable for production
