304d16338ab3ea638b14412f8a07e8dddd86da4f
junh.kim <junh.kim@ohmytrip.com>
2017-01-06 15:13:41 +0900

    .

--- authome/settings.py
+++ authome/settings.py
@@ -93,7 +93,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.core.context_processors.media',
+                # 'django.core.context_processors.media',
             ],
         },
     },
