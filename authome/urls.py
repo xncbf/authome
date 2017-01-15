ca7822281890b6e72412e1d3979441831d1d3e0e
Joon Hwan 김준환 <xncbf12@gmail.com>
2017-01-15 18:54:00 +0900

    404 핸들러 생성

--- authome/urls.py
+++ authome/urls.py
@@ -19,6 +19,12 @@ from mypage import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+handler404 = 'mypage.views.page_not_found_view'
+# handler500 = 'mypage.views.error_view'
+# handler403 = 'mypage.views.permission_denied_view'
+# handler400 = 'mypage.views.bad_request_view'
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/', views.user_login, name='login'),
