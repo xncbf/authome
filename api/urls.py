0b8840c84cfcc680548d161daff72e31e10cade9
junh.kim <junh.kim@ohmytrip.com>
2017-01-25 16:10:05 +0900

    fixed error url

--- api/urls.py
+++ api/urls.py
@@ -11,6 +11,6 @@ handler400 = 'mypage.views.bad_request_view'
 urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^(?P<macro_id>[0-9a-z-]+)/$', views.UserPageDetail.as_view()),
-    url(r'^/$', bad_gateway()),
+    url(r'^$', bad_gateway()),
     # url(r'^$', views.UserPageList.as_view()),
 ]
