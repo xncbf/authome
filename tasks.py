ee1c0a37b2685a1b84aff88f4a14a4141412d4ed
junh.kim <junh.kim@ohmytrip.com>
2017-01-25 16:39:19 +0900

    add invoke

---
+++ tasks.py
@@ -0,0 +1,28 @@
+import sys
+import time
+from urllib.request import urlopen
+from invoke import Collection
+from invoke import task
+
+from invoke_tasks.helpers import is_task_file_changed
+
+
+@task
+def deploy(ctx):
+    pull_result = ctx.run("git pull origin master")
+    if is_task_file_changed(pull_result):
+        sys.exit("Pyinvoke task file(s) is changed. Please re-run this task.")
+    ctx.run("pip install -r requirements.txt")
+    ctx.run("python manage.py collectstatic --noinput")
+    # Restart receiving server and batch server.
+    ctx.run("sudo service uwsgi restart", pty=True)
+    # Give some time for reloading application.
+    time.sleep(1)
+    # Test if the site works well.
+    with urlopen("http://autho.me") as response:
+        if not response.getcode() == 200:
+            sys.exit("CRITICAL: The site respond CODE " + response.getcode())
+    # Success!
+    print("Deploy succeeded.")
+
+namespace = Collection(deploy)
