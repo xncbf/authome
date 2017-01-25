34084ea9acf25a6d9aed3e92d0dd517effc10b98
junh.kim <junh.kim@ohmytrip.com>
2017-01-25 16:09:02 +0900

    edit 502 error page

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import url, include
 from . import views
+from ..mypage.views import bad_gateway
 
 
 handler404 = 'mypage.views.page_not_found_view'
 handler500 = 'mypage.views.error_view'
-handler502 = 'mypage.views.error_view'
 handler403 = 'mypage.views.permission_denied_view'
 handler400 = 'mypage.views.bad_request_view'
 
 urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^(?P<macro_id>[0-9a-z-]+)/$', views.UserPageDetail.as_view()),
+    url(r'^/$', bad_gateway()),
     # url(r'^$', views.UserPageList.as_view()),
 ]
