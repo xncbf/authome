47dacee1bac384b21cf6baf805d64f5a68a8a52e
junh.kim <junh.kim@ohmytrip.com>
2017-01-26 14:45:46 +0900

    change task order

--- tasks.py
+++ tasks.py
@@ -12,8 +12,9 @@ def deploy(ctx):
     pull_result = ctx.run("git pull origin master")
     if is_task_file_changed(pull_result):
         sys.exit("Pyinvoke task file(s) is changed. Please re-run this task.")
-    ctx.run("python manage.py migrate")
     ctx.run("pip install -r requirements.txt")
+    ctx.run("python manage.py makemigrations")
+    ctx.run("python manage.py migrate")
     ctx.run("python manage.py collectstatic --noinput")
     ctx.run("sudo cp /root/authome/api/templates/error/502.html /tmp/authome/")
     # Restart receiving server and batch server.
