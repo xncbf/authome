209fbb7f8f44bfb41e1d0c3af572d6bff5d4c446
Joon Hwan 김준환 <xncbf12@gmail.com>
2017-02-06 22:29:22 +0900

    add site_id to dev

--- authome/settings_local.py
+++ authome/settings_local.py
@@ -5,7 +5,7 @@ SECRET_KEY = '1tcc-!d(x+=nj9@60dzuqo7$$(4v632-p=m%6y1ee!o!9fiepx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+SITE_ID = 3
 ALLOWED_HOSTS = ['*']
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
