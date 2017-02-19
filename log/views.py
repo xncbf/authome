9c2efd8fe0aa0634bef0584a945ac176eff93a5e
Joon Hwan 김준환 <xncbf12@gmail.com>
2017-02-19 21:37:50 +0900

    ajax get response

--- log/views.py
+++ log/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views.generic.list import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from main.models import MacroLog
+from django.core.serializers import serialize
+from main.models import MacroLog, UserPage
 
 
 class Log(LoginRequiredMixin, View):
     template_name = "log/log.html"
     login_url = '/accounts/login/'
+
     def get(self, request, *args, **kwargs):
         context = {}
-        context['macroLog'] = MacroLog.objects.filter(macro__user=request.user)
+        context['macroLog'] = MacroLog.objects.filter(macro__user=request.user)[:8]
+        context['userPage'] = UserPage.objects.filter(macro__user=request.user).distinct('user')
         return render(self.request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        if request.is_ajax():
+            ddlUser = request.POST.get('ddlUser')
+            data = serialize("json", MacroLog.objects.filter(macro__user__id__in=ddlUser.split(','))[:8])
+            return HttpResponse(data)
