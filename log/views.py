a716599a815e4bed799b4cbfd696b2c09a5df3c4
Joon Hwan 김준환 <xncbf12@gmail.com>
2017-02-19 17:57:20 +0900

    modified log page

--- log/views.py
+++ log/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from main.models import MacroLog
 
 
 class Log(LoginRequiredMixin, View):
     template_name = "log/main.html"
     login_url = '/accounts/login/'
-
-    def get(self, *args, **kwargs):
-        return render(self.request, self.template_name)
+    def get(self, request, *args, **kwargs):
+        context = {}
+        context['macroLog'] = MacroLog.objects.filter(macro__user=request.user)
+        return render(self.request, self.template_name, context)
--- main/static/js/init.js
+++ main/static/js/init.js
@@ -12,6 +12,8 @@
             selectYears: 15, // Creates a dropdown of 15 years to control year
             format: 'yyyy-mm-dd'
         });
-
+        $(document).ready(function () {
+            $('select').material_select();
+        });
     }); // end of document ready
 })(jQuery); // end of jQuery name space
