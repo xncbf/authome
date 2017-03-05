4054edaf684ea0a1adc1e42c1fcee1beb5cea665
Joon Hwan 김준환 <xncbf12@gmail.com>
2017-03-05 21:48:11 +0900

    로그 social profile url 적용

--- log/views.py
+++ log/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic.list import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.db import connection
 from django.core.urlresolvers import reverse
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.utils.dateformat import DateFormat
+from allauth.socialaccount.models import SocialAccount
 from main.models import MacroLog, UserPage
 from .services import dictfetchall
 
@@ -45,6 +47,11 @@ class Log(LoginRequiredMixin, View):
 
     def set_html(self, obj, html=''):
         for e in obj:
+            user = User.objects.get(username=e.get('username'))
+            if user.socialaccount_set.all():
+                profile_url = user.socialaccount_set.all()[0].get_avatar_url()
+            else:
+                profile_url = static('images/Jigglypuff.png')
             html += """<li class="collection-item user-list">
                         <a href="{0}">
                             <div>{1}</div>
@@ -54,10 +61,10 @@ class Log(LoginRequiredMixin, View):
                             <span class="secondary-content">{4}<br>{5}</span>
                         </a>
                     </li>""".format(reverse('main:macro_manager', kwargs={'macro_id': e.get('macro_id')}),
-                                     e.get('title') or '제목없음',
-                                     static('images/Jigglypuff.png'),
-                                     e.get('username'),
-                                     e.get('ip'),
+                                    e.get('title') or '제목없음',
+                                    profile_url,
+                                    e.get('username'),
+                                    e.get('ip'),
                                     DateFormat(e.get('created')).format('y-m-d H:i'))
         if len(obj) == 0:
             html = '<li class="collection-item user-list">사용 흔적이 없어요!</li>'
