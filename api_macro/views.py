4c5e9ad105c222b92ca5db76d56cde1b9eaffd29
Joon Hwan 김준환 <xncbf12@gmail.com>
2017-03-10 22:11:01 +0900

    토큰으로 가져오기 없앰

--- api_macro/views.py
+++ api_macro/views.py
@@ -37,29 +37,4 @@ class GetAuth(APIView):
             MacroLog.objects.create(user=user, macro=userPage.macro, ip=get_ip(request))
             return Response(serializer.data)
         else:
-            try:
-                social_user = User.objects.get(username=username)
-                social_account = SocialAccount.objects.get(user_id=social_user.pk)
-                social_token = SocialToken.objects.get(account_id=social_account.pk)
-                if social_token.token == password:
-                    userPage = self.get_object(social_user.username, macro_id)
-                    serializer = AuthSerializer(userPage)
-                    MacroLog.objects.create(user=social_user, macro=userPage.macro, ip=get_ip(request))
-                    return Response(serializer.data)
-                else:
-                    raise Exception
-            except:
-                return Response(status=status.HTTP_403_FORBIDDEN)
-
-
-class GetList(APIView):
-    """
-    전체 Endpoint 를 가져옵니다.
-    """
-    def get(self, request, format=None):
-        userPage = UserPage.objects.all()
-        # if not len(userPage):
-        #     raise Http404
-        serializer = AuthSerializer(userPage)
-        MacroLog.objects.create(user=request.user, macro=userPage.macro, ip=get_ip(request))
-        return Response(serializer.data)
+            return Response(status=status.HTTP_403_FORBIDDEN)
