4a8f247c8e91b3f68b2ab37a1df357f03e14078b
Joon Hwan 김준환 <xncbf12@gmail.com>
2017-03-11 19:21:43 +0900

    ready for creating sitemap

--- authome/urls.py
+++ authome/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from main import views
 from django.conf.urls.static import static
 from django.conf import settings
