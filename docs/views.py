97e4a0cd6b34503131f58aa440d291d6c917667f
Joon Hwan 김준환 <xncbf12@gmail.com>
2017-03-16 22:29:55 +0900

    오토핫키 활용 예제 문서 작성

--- docs/views.py
+++ docs/views.py
@@ -7,5 +7,9 @@ def quick_start(request):
     return render(request, 'docs/quickstart.html')
 
 
+def use_auth(request):
+    return render(request, 'docs/use/autohotkey.html')
+
+
 def introduction(request):
     return render(request, 'docs/introduction.html')
