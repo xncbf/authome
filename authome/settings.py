9d529a0410c0da40c3fb658aa7223a7cc6ca48a7
Joon Hwan 김준환 <xncbf12@gmail.com>
2017-04-02 14:02:48 +0900

    test용 crontab

--- authome/settings.py
+++ authome/settings.py
@@ -61,7 +61,8 @@ CELERY_RESULT_BACKEND = 'amqp://'
 CELERYBEAT_SCHEDULE = {
     'get_ses_statistics': {
         'task': 'authome.tasks.get_ses_statistics',
-        'schedule': crontab(hour=0, minute=0),
+        'schedule': crontab(minute='*'),
+        # 'schedule': crontab(hour=0, minute=0),
     },
 }
 
