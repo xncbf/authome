8c911eb9bfa3c0a15b0eb2bec9fa92625f1196c9
xncbf <xncbf12@gmail.com>
2017-04-03 15:06:24 +0900

    added @task

--- authome/tasks.py
+++ authome/tasks.py
@@ -9,6 +9,7 @@ def get_ses_statistics():
     call_command('get_ses_statistics')
 
 
+@app.task
 def verify_end_yn():
     UserPage.objects.filter(end_date__lt=timezone.now(), end_yn=True).update(end_yn=False)
     UserPage.objects.filter(end_date__gte=timezone.now(), end_yn=False).update(end_yn=True)
