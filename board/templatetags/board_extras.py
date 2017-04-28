a82452c31d0f7dc50bc4b0c3b60a90e3c5c62847
xncbf <xncbf12@gmail.com>
2017-04-28 15:51:04 +0900

    added customfilters

---
+++ board/templatetags/board_extras.py
@@ -0,0 +1,26 @@
+from django import template
+from django.utils import timezone
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from ipware.ip import is_valid_ip
+
+register = template.Library()
+
+
+@register.filter(name='truncateip')
+def truncate_ip(value):
+    result = ''
+    if is_valid_ip(value):
+        result = ".".join(value.split('.')[:-1])
+        result += ".xxx"
+    else:
+        result = '유효하지 않은 아이피입니다.'
+    return result
+
+
+@register.filter(name='naturaltime_newly')
+def natural_time_newly(value):
+    # 오늘로부터 하루 이상 지난 조건
+    if (timezone.now() - value).days >= 1:
+        return value
+    else:
+        return naturaltime(value)
