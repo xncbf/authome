993a4f19ba30f53854272e0c72edd5f32d6aa03f
Joon Hwan 김준환 <xncbf12@gmail.com>
2017-12-25 23:11:48 +0900

    django 1.11

--- authome/settings.py
+++ authome/settings.py
@@ -123,6 +123,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_THROTTLE_CLASSES': (
         'rest_framework.throttling.AnonRateThrottle',
