a986be5a674a6a360260cfe7c4c0fe2caefe6ef8
xncbf <xncbf12@gmail.com>
2017-12-26 23:57:34 +0900

    zappa

--- authome/tasks.py
+++ authome/tasks.py
@@ -3,12 +3,10 @@ from django.utils import timezone
 from main.models import UserPage
 
 
-@app.task
 def get_ses_statistics():
     call_command('get_ses_statistics')
 
 
-@app.task
 def verify_end_yn():
     UserPage.objects.filter(end_date__lt=timezone.now(), end_yn=True).update(end_yn=False)
     UserPage.objects.filter(end_date__gte=timezone.now(), end_yn=False).update(end_yn=True)
