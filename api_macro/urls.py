df4729bffb7eb1864002166e0ba32f3194c1e084
xncbf <xncbf12@gmail.com>
2018-01-13 00:24:22 +0900

    .

--- api_macro/urls.py
+++ api_macro/urls.py
@@ -8,5 +8,7 @@ handler403 = 'main.views.permission_denied_view'
 handler400 = 'main.views.bad_request_view'
 
 urlpatterns = [
+    # 17년 5월부터 사용금지인데 기존에 쓰고있는 사람때문에 지울수가없다 ㅠ
+    url(r'^auth/(?P<username>.*)/(?P<password>.*)/(?P<macro_id>[0-9a-z-]+)/$', views.GetAuth.as_view()),
     url(r'^auth/(?P<token>.*)/(?P<macro_id>[0-9a-z-]+)/$', views.GetAuth2.as_view()),
 ]
