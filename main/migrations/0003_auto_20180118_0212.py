d47d9424c9fa751e787830abc5e6f304ccfdd4ff
xncbf <xncbf12@gmail.com>
2018-01-18 23:11:21 +0900

    refactor

--- main/migrations/0003_auto_20180118_0212.py
+++ main/migrations/0003_auto_20180118_0212.py
@@ -18,11 +18,16 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='extendsuser',
             name='nickname',
-            field=models.CharField(error_messages={'unique': '이미 사용중인 닉네임입니다.'}, help_text='필수 항목. 10자 이하. 문자와 숫자만 가능.', max_length=10, null=True, unique=True, validators=[django.core.validators.RegexValidator('^[\\w]+$', '유효한 닉네임을 입력해주세요. 이 항목은® 문자와 숫자만 사용 가능합니다.')], verbose_name='닉네임'),
+            field=models.CharField(
+                error_messages={'unique': '이미 사용중인 닉네임입니다.'}, help_text='필수 항목. 10자 이하. 문자와 숫자만 가능.',
+                max_length=10, null=True, unique=True, validators=[django.core.validators.RegexValidator(
+                    '^[\\w]+$', '유효한 닉네임을 입력해주세요. 이 항목은® 문자와 숫자만 사용 가능합니다.')],
+                verbose_name='닉네임'),
         ),
         migrations.AlterField(
             model_name='extendsuser',
             name='user',
-            field=models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='extendsuser', to=settings.AUTH_USER_MODEL),
+            field=models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='extendsuser',
+                                       to=settings.AUTH_USER_MODEL),
         ),
     ]
