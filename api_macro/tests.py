f54160d4ef8a0fefe86a540238a7063d8001544c
xncbf <xncbf12@gmail.com>
2018-01-20 23:54:24 +0900

    test

--- api_macro/tests.py
+++ api_macro/tests.py
@@ -2,25 +2,26 @@ from datetime import timedelta
 
 from django.contrib.auth.models import User
 from django.utils import timezone
-from rest_framework.test import APITestCase
-from subdomains.utils import reverse
+from rest_framework.test import APITestCase, APIRequestFactory
 
 from main.models import Macro, UserPage
+from .views import GetAuth2
 
 
 class Tests(APITestCase):
     def setUp(self):
         user = User.objects.create(email="xncbf12@gmail.com")
         macro = Macro.objects.create(user=user)
-        UserPage.objects.create(user=user, macro=macro, end_date=timezone.now() + timedelta(days=1))
+        self.time = timezone.now() + timedelta(days=1)
+        UserPage.objects.create(user=user, macro=macro, end_date=self.time)
 
     def test_success_request(self):
         testing_account = User.objects.get(email='xncbf12@gmail.com')
         token = testing_account.extendsuser.token
         macro_id = Macro.objects.get(user=testing_account).id
-        response = self.client.get(reverse('accounts:auth', subdomain='api', kwargs={
-            'token': str(token), 'macro_id': macro_id}))
-        print(reverse('accounts:auth', subdomain='api', kwargs={
-            'token': str(token), 'macro_id': macro_id}))
-        print(response.content)
-        self.assertEqual(response.status_code, 200)
+
+        factory = APIRequestFactory()
+        view = GetAuth2.as_view()
+        request = factory.get('/')
+        response = view(request, token=str(token), macro_id=macro_id)
+        self.assertEqual(response.data.get('end_yn'), True)
