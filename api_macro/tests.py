4a15c2fefce2852c6227b5757db06d6314ee7866
xncbf <xncbf12@gmail.com>
2018-01-20 23:08:50 +0900

    test

--- api_macro/tests.py
+++ api_macro/tests.py
@@ -2,14 +2,14 @@ from datetime import timedelta
 
 from django.contrib.auth.models import User
 from django.utils import timezone
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase
+from subdomains.utils import reverse
 
 from main.models import Macro, UserPage
 
 
 class Tests(APITestCase):
     def setUp(self):
-        self.client = APIClient()
         user = User.objects.create(email="xncbf12@gmail.com")
         macro = Macro.objects.create(user=user)
         UserPage.objects.create(user=user, macro=macro, end_date=timezone.now() + timedelta(days=1))
@@ -18,5 +18,9 @@ class Tests(APITestCase):
         testing_account = User.objects.get(email='xncbf12@gmail.com')
         token = testing_account.extendsuser.token
         macro_id = Macro.objects.get(user=testing_account).id
-        response = self.client.get('/macro/auth/%s/%s/' % (str(token), macro_id))
+        response = self.client.get(reverse('accounts:auth', subdomain='api', kwargs={
+            'token': str(token), 'macro_id': macro_id}))
+        print(reverse('accounts:auth', subdomain='api', kwargs={
+            'token': str(token), 'macro_id': macro_id}))
+        print(response.content)
         self.assertEqual(response.status_code, 200)
